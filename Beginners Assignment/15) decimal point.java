33a15b834eb79feddd742c5bacb2fe9023c98c27
nihar2103 <108277276+nihar2103@users.noreply.github.com>
2022-09-13 09:31:16 +0530

    Update 15) decimal point.java

--- Beginners Assignment/15) decimal point.java
+++ Beginners Assignment/15) decimal point.java
@@ -1,1 +1,9 @@
-
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float n=sc.nextFloat();
+        int m=sc.nextInt();
+        System.out.println(n+m);
+    }
+}
